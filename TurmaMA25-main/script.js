e96f6eaf8f405b156fbbcf13c33f15212b601b9c
__hellen.Timão <164502133+hellencorinthias@users.noreply.github.com>
2025-03-21 09:34:48 -0300

    .

--- TurmaMA25-main/script.js
+++ TurmaMA25-main/script.js
@@ -1,4 +1,4 @@
-# TurmaMA202document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
  
